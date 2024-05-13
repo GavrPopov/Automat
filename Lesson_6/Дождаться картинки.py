7473d009c75281d076614f28a3bd0830a1b4ca99
GavrPopov <150421338+GavrPopov@users.noreply.github.com>
2024-05-13 23:00:25 +0500

    Update Дождаться картинки.py

--- Lesson_6/Дождаться картинки.py
+++ Lesson_6/Дождаться картинки.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-
 wait = WebDriverWait(driver, 20)
 wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '#landscape')))
 src = driver.find_element(By.CSS_SELECTOR, '#award')
