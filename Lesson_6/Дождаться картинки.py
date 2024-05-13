ba6f3a82ae9e4f8287c1d4c6eb5f2b881cc2b5c9
GavrPopov <150421338+GavrPopov@users.noreply.github.com>
2024-05-13 23:00:04 +0500

    Update Дождаться картинки.py

--- Lesson_6/Дождаться картинки.py
+++ Lesson_6/Дождаться картинки.py
@@ -3,17 +3,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Открываем браузер и переходим на страницу
 driver = webdriver.Chrome()
+
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-# Ждем загрузки всех картинок
-wait = WebDriverWait(driver, 10)
-images = wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "img")))
+wait = WebDriverWait(driver, 20)
+wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '#landscape')))
+src = driver.find_element(By.CSS_SELECTOR, '#award')
 
-# Получаем значение атрибута src у 3-й картинки
-third_image_src = images[3].get_attribute("src")
-print(third_image_src)
-
-# Закрываем браузер
-driver.quit()
+print(src.get_attribute('src'))
