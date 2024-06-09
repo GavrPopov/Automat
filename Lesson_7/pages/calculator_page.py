0252928fa42244b2ab48196c0ed29cce809dc750
Gavr Popov <gp-92@mail.ru>
2024-06-09 19:09:41 +0500

    HW7_1

--- Lesson_7/pages/calculator_page.py
+++
@@ -1,6 +0,0 @@
-class CalculatorPage:
-    def __init__(self, driver):
-        self._driver = driver
-
-    def open(self, url):
-        self._driver.get(url)
--- Lesson_7/pages/shopping_page.py
+++
@@ -1,7 +0,0 @@
-class LoginPage: 
-    def __init__(self, driver):
-        self._driver = driver
-
-    def open(self, url):
-        self._driver.get(url)
-
