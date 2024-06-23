a2b9c87a0d7bc79c06d33677df57d86ad2fd2eb5
Gavr Popov <gp-92@mail.ru>
2024-06-23 21:08:29 +0500

    ok

---
+++ Diplom/Config.py
@@ -0,0 +1,4 @@
+Site_URL = "web-gate.chitai-gorod.ru/api/v1"
+Site_URL_2 = "web-gate.chitai-gorod.ru/api/v2"
+TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIwODA1NTQwLCJpYXQiOjE3MTkxNTU2MjcsImV4cCI6MTcxOTE1OTIyNywidHlwZSI6MjB9.5rrj3RDngKpDdloL8tbWNmIb567IO14aiHN84xXwn90"
+id = "harry-potter-and-the-philosophers-stone-2444651"
