a3539c817c5e59a4c18bfd4860842c518adaf0ee
GavrPopov <150421338+GavrPopov@users.noreply.github.com>
2024-07-01 00:23:26 +0500

    Delete Diplom/Config.py

--- Diplom/Config.py
+++
@@ -1,3 +0,0 @@
-Site_URL = "web-gate.chitai-gorod.ru/api/v1"
-Site_URL_2 = "web-gate.chitai-gorod.ru/api/v2"
-TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VzZXItcmlnaHQiLCJzdWIiOjIwODA1NTQwLCJpYXQiOjE3MTkxNjgyMjMsImV4cCI6MTcxOTE3MTgyMywidHlwZSI6MjB9.fxirTyiIjnj04RKjq3TvRx0JnTP9Q3kXFvhzYUju2-8"
